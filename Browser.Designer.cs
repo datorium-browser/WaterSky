c9fa647ca3db1c776407bdd2401a1389a784fa28
Strykeros <Strykeros-PC>
2021-03-19 14:57:41 +0200

    Adding Menu Window

--- Browser.Designer.cs
+++ Browser.Designer.cs
@@ -34,10 +34,11 @@
             this.AddressBar = new System.Windows.Forms.ToolStripTextBox();
             this.ButtonAddTab = new System.Windows.Forms.ToolStripButton();
             this.ButtonTabRemove = new System.Windows.Forms.ToolStripButton();
+            this.historyButton = new System.Windows.Forms.ToolStripButton();
             this.BrowserTabs = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.historyButton = new System.Windows.Forms.ToolStripButton();
+            this.menuButton = new System.Windows.Forms.ToolStripButton();
             this.toolStrip1.SuspendLayout();
             this.BrowserTabs.SuspendLayout();
             this.SuspendLayout();
@@ -50,10 +51,11 @@
             this.AddressBar,
             this.ButtonAddTab,
             this.ButtonTabRemove,
-            this.historyButton});
+            this.historyButton,
+            this.menuButton});
             this.toolStrip1.Location = new System.Drawing.Point(0, 0);
             this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Size = new System.Drawing.Size(600, 39);
+            this.toolStrip1.Size = new System.Drawing.Size(831, 39);
             this.toolStrip1.TabIndex = 0;
             this.toolStrip1.Text = "toolStrip1";
             // 
@@ -97,41 +99,6 @@
             this.ButtonTabRemove.Text = "-";
             this.ButtonTabRemove.Click += new System.EventHandler(this.ButtonTabRemove_Click);
             // 
-            // BrowserTabs
-            // 
-            this.BrowserTabs.Controls.Add(this.tabPage1);
-            this.BrowserTabs.Controls.Add(this.tabPage2);
-            this.BrowserTabs.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.BrowserTabs.Location = new System.Drawing.Point(0, 39);
-            this.BrowserTabs.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.BrowserTabs.Name = "BrowserTabs";
-            this.BrowserTabs.SelectedIndex = 0;
-            this.BrowserTabs.Size = new System.Drawing.Size(600, 327);
-            this.BrowserTabs.TabIndex = 1;
-            this.BrowserTabs.Selected += new System.Windows.Forms.TabControlEventHandler(this.BrowserTabs_Selected);
-            // 
-            // tabPage1
-            // 
-            this.tabPage1.Location = new System.Drawing.Point(4, 22);
-            this.tabPage1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.tabPage1.Size = new System.Drawing.Size(592, 301);
-            this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "tabPage1";
-            this.tabPage1.UseVisualStyleBackColor = true;
-            // 
-            // tabPage2
-            // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
-            this.tabPage2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.tabPage2.Size = new System.Drawing.Size(592, 301);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
-            this.tabPage2.UseVisualStyleBackColor = true;
-            // 
             // historyButton
             // 
             this.historyButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
@@ -142,16 +109,62 @@
             this.historyButton.Text = "History";
             this.historyButton.Click += new System.EventHandler(this.historyButton_Click);
             // 
+            // BrowserTabs
+            // 
+            this.BrowserTabs.Controls.Add(this.tabPage1);
+            this.BrowserTabs.Controls.Add(this.tabPage2);
+            this.BrowserTabs.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.BrowserTabs.Location = new System.Drawing.Point(0, 39);
+            this.BrowserTabs.Margin = new System.Windows.Forms.Padding(2);
+            this.BrowserTabs.Name = "BrowserTabs";
+            this.BrowserTabs.SelectedIndex = 0;
+            this.BrowserTabs.Size = new System.Drawing.Size(831, 507);
+            this.BrowserTabs.TabIndex = 1;
+            this.BrowserTabs.Selected += new System.Windows.Forms.TabControlEventHandler(this.BrowserTabs_Selected);
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(2);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(2);
+            this.tabPage1.Size = new System.Drawing.Size(823, 481);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Margin = new System.Windows.Forms.Padding(2);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(2);
+            this.tabPage2.Size = new System.Drawing.Size(592, 301);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // menuButton
+            // 
+            this.menuButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.menuButton.Image = ((System.Drawing.Image)(resources.GetObject("menuButton.Image")));
+            this.menuButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.menuButton.Name = "menuButton";
+            this.menuButton.Size = new System.Drawing.Size(42, 36);
+            this.menuButton.Text = "Menu";
+            this.menuButton.Click += new System.EventHandler(this.menuButton_Click);
+            // 
             // Browser
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.ClientSize = new System.Drawing.Size(831, 546);
             this.Controls.Add(this.BrowserTabs);
             this.Controls.Add(this.toolStrip1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Browser";
             this.Text = "Form1";
+            this.LocationChanged += new System.EventHandler(this.Browser_LocationChanged);
             this.toolStrip1.ResumeLayout(false);
             this.toolStrip1.PerformLayout();
             this.BrowserTabs.ResumeLayout(false);
@@ -171,6 +184,7 @@
         private System.Windows.Forms.ToolStripButton ButtonAddTab;
         private System.Windows.Forms.ToolStripButton ButtonTabRemove;
         private System.Windows.Forms.ToolStripButton historyButton;
+        private System.Windows.Forms.ToolStripButton menuButton;
     }
 }
 
